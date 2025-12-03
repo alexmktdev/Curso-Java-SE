c1790dbfb22f8b110c95081d59ae54757ab28118
Alexander Ruiz Poblete <alexanderzananiri26@gmail.com>
2025-12-03 14:21:51 -0300

    se agrega el ejemplo1 holamundo en java

---
+++ src/ejemplo_01_holamundo.java
@@ -0,0 +1,8 @@
+
+// ejemplo 1 - hola mundo en java
+
+public class ejemplo_01_holamundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo en Java !");
+    }
+}
